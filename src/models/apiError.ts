2acc28aacf308e8e55a61b0d3be26c0b628095ed
John Doe <hamedhasan422421@gmail.com>
2023-06-03 19:05:32 +0300

    setup validation errors

--- src/models/apiError.ts
+++ src/models/apiError.ts
@@ -1,11 +1,15 @@
-export class apiError extends Error {
-    statusCode: number; // Add the statusCode property
-  
-    constructor(message: string, statusCode: number) {
-      super(message);
-      this.statusCode = statusCode;
-      this.name = this.constructor.name;
-      Error.captureStackTrace(this, this.constructor);
+class ApiError extends Error {
+  statusCode: number
+
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
     }
   }
-  
+}
+
+export default ApiError
